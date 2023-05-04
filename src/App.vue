8a619d0c9f742ac11e93a0718981d260d2c946c0
Vitaly <vitalic.web@gmail.com>
2023-05-04 21:14:16 +0300

    Add fill action to get products

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
-import {useProductStore} from "@/stores/ProductStore";
+import { useProductStore } from "@/stores/ProductStore";
 
-useProductStore();
+const productStore = useProductStore();
+productStore.fill();
 </script>
 
 <template>
@@ -12,7 +12,7 @@ useProductStore();
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
-        v-for="product in products"
+        v-for="product in productStore.products"
         :key="product.name"
         :product="product"
       />
