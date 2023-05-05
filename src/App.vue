64ab61cf2a1844bc65820c1c27d77ada00227acd
Vitaly <vitalic.web@gmail.com>
2023-05-05 19:19:46 +0300

    Create CartStore

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "@/stores/ProductStore";
+import { useCartStore } from "@/stores/CartStore";
 
 const productStore = useProductStore();
+const cartStore = useCartStore();
 productStore.fill();
 </script>
 
@@ -15,6 +17,7 @@ productStore.fill();
         v-for="product in productStore.products"
         :key="product.name"
         :product="product"
+				@addToCart="cartStore.addItems($event, product)"
       />
     </ul>
   </div>
