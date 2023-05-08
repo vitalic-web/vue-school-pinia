657c2e3068a7483ec8af27132a973722c433cb48
Vitaly <vitalic.web@gmail.com>
2023-05-08 17:28:48 +0300

    Add redo from cart method

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,24 @@ const cartStore = useCartStore();
 productStore.fill();
 
 const history = reactive([]);
+const future = reactive([]);
 const doingHistory = ref(false);
 
 history.push(JSON.stringify(cartStore.$state));
 
+const redo = () => {
+  const latestState = future.pop();
+  if (!latestState) return;
+  doingHistory.value = true;
+  history.push(latestState);
+  cartStore.$state = JSON.parse(latestState);
+  doingHistory.value = false;
+};
+
 const undo = () => {
   if (history.length === 1) return;
   doingHistory.value = true;
-  history.pop();
+  future.push(history.pop());
   cartStore.$state = JSON.parse(history.at(-1));
   doingHistory.value = false;
 };
@@ -26,6 +36,7 @@ const undo = () => {
 cartStore.$subscribe((mutation, state) => {
   if (!doingHistory.value) {
     history.push(JSON.stringify(state));
+    future.splice(0, future.length);
 	}
 });
 
@@ -52,6 +63,7 @@ cartStore.$onAction(({
     <TheHeader />
 		<div class="mb-5 flex justify-end">
 			<AppButton @click="undo">Undo</AppButton>
+			<AppButton class="ml-2" @click="redo">Redo</AppButton>
 		</div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
